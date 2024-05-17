144b626a17d03da9a6b7c03c65fdaf35f8f1f511
Puputera <127200098+Puputera@users.noreply.github.com>
2024-05-17 08:34:11 +0700

    Update data.blade.php

--- resources/views/admin/data.blade.php
+++ resources/views/admin/data.blade.php
@@ -10,13 +10,11 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{-- {{ __("DATA PENDAFTARAN MAHASISWA") }} --}}
-
-
                     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
                         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                             <thead
                                 class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                                <tr>
+                                <tr style="text-align: center;">
                                     <th scope="col" class="px-6 py-3">
                                         Nama
                                     </th>
@@ -36,17 +34,14 @@
                                         Persetujuan
                                     </th>
                                     <th scope="col" class="px-6 py-3">
-                                        Aksi Persetujuan
+                                        Aksi
                                     </th>
                                 </tr>
                             </thead>
                             <tbody>
                                 @foreach ($mahasiswa as $item)
-                                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                        <th scope="row"
-                                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                            {{ $item->id }}
-                                        </th>
+                                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                                        style="text-align: center;">
                                         <td class="px-6 py-4">
                                             {{ $item->nama_mhs }}
                                         </td>
@@ -55,6 +50,9 @@
                                         </td>
                                         <td class="px-6 py-4">
                                             {{ $item->email }}
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            {{ $item->no_telp }}
                                         </td>
                                         <td class="px-6 py-4">
                                             @if ($item->prodi == 'ti')
@@ -88,17 +86,24 @@
                                             @endif
                                         </td>
                                         <td class="px-6 py-4">
-                                            <form action="{{ route('update.status') }}" method="POST">
+                                            <form action="{{ route('update.status') }}" method="POST" class="inline">
                                                 @csrf
                                                 <input type="hidden" name="id" value="{{ $item->id }}">
                                                 <button type="submit" name="status" value="diterima"
-                                                    class="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">DITERIMA</button>
+                                                    class="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">TERIMA</button>
                                             </form>
-                                            <form action="{{ route('update.status') }}" method="POST">
+                                            <form action="{{ route('update.status') }}" method="POST" class="inline">
                                                 @csrf
                                                 <input type="hidden" name="id" value="{{ $item->id }}">
                                                 <button type="submit" name="status" value="ditolak"
-                                                    class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">DITOLAK</button>
+                                                    class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">TOLAK</button>
+                                            </form>
+                                            <form action="{{ route('admin.hapus') }}" method="POST" class="inline"
+                                                onsubmit="return confirm('Apakah Anda yakin ingin menghapus data ini?');">
+                                                @csrf
+                                                <input type="hidden" name="id" value="{{ $item->id }}">
+                                                <button type="submit"
+                                                    class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">HAPUS</button>
                                             </form>
                                         </td>
                                     </tr>
